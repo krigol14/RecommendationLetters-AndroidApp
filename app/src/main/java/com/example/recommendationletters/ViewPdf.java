e7ae93b2c49cc67a583d89623bcaef5f535118b3
Kristian Golemi <krigol0014@gmail.com>
2022-03-02 00:10:01 +0200

    Configured code regarding the professor's ability to sign pdf documents

--- app/src/main/java/com/example/recommendationletters/ViewPdf.java
+++ app/src/main/java/com/example/recommendationletters/ViewPdf.java
@@ -1,15 +1,23 @@
 package com.example.recommendationletters;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -19,7 +27,7 @@ public class ViewPdf extends AppCompatActivity {
     String urls;
     PDFView pdfView;
     ProgressDialog dialog;
-    Button sign, grant;
+    Button grant, save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +35,18 @@ public class ViewPdf extends AppCompatActivity {
         setContentView(R.layout.activity_view_pdf);
         pdfView = findViewById(R.id.abc);
 
-        sign = findViewById(R.id.sign);
+        RelativeLayout parent = (RelativeLayout) findViewById(R.id.signpdf);
+        MyDrawView myDrawView = new MyDrawView(this);
+        parent.addView(myDrawView);
+
+        // change the action bar's color
+        ActionBar actionBar = getSupportActionBar();
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#284b63"));
+        assert actionBar != null;
+        actionBar.setBackgroundDrawable(colorDrawable);
+
         grant = findViewById(R.id.grant);
+        save = findViewById(R.id.save_sign);
 
         // show progress while loading the pdf file
         dialog = new ProgressDialog(this);
@@ -37,20 +55,33 @@ public class ViewPdf extends AppCompatActivity {
 
         // get the url of the pdf
         urls = getIntent().getStringExtra("url");
-        new RetrivePdfStream().execute(urls);
+        new RetrievePdfStream().execute(urls);
 
-        // when professor presses the sign button , redirect him to another activity
-        // where he can sign and his signature will be saved as a png to be later added on the pdf
-        sign.setOnClickListener(new View.OnClickListener() {
+        save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ViewPdf.this, Sign.class));
+                parent.setDrawingCacheEnabled(true);
+                Bitmap bmp = parent.getDrawingCache();
+
+                // save the signature in .png format in Device File Explorer data/data/com.example.recommendationLetters/files/signature.png
+                try {
+                    FileOutputStream fos = openFileOutput("idea_signature.png", Context.MODE_PRIVATE);
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    bmp.compress(Bitmap.CompressFormat.PNG, 100 , bos);
+                    byte[] bitmapdata = bos.toByteArray();
+                    fos.write(bitmapdata);
+                    fos.flush();
+                    fos.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
     // retrieve the pdf using its url
-    class RetrivePdfStream extends AsyncTask<String, Void, InputStream> {
+    class RetrievePdfStream extends AsyncTask<String, Void, InputStream> {
         @Override
         protected InputStream doInBackground(String... strings) {
             InputStream inputStream = null;
